3ef6e71fe378507a3133a9833d6c8c76f97a0611
한예슬 <letmeshare46@gmail.com>
2022-07-28 17:39:52 +0900

    norm check

--- src/executer/exec_command.h
+++ src/executer/exec_command.h
@@ -1,18 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_command.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: han-yeseul <han-yeseul@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 17:38:29 by han-yeseul        #+#    #+#             */
+/*   Updated: 2022/07/28 17:38:55 by han-yeseul       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef EXEC_COMMAND_H
-#define EXEC_COMMAND_H
+# define EXEC_COMMAND_H
 
 /*    exec_command.c    */
-void	exec_priority_operator(t_tree *node, t_token *tok, int *status, t_shell_config *config);
+void	exec_priority_operator(t_tree *node, t_token *tok, int *status, \
+			t_shell_config *config);
 int		exec_general(t_tree *node, char **cmd_argv, t_shell_config *config);
 int		exec_exceptions(t_tree *node, char **cmd_argv, t_shell_config *config);
 int		exec_builtin(char **cmd_argv, char ***envp);
 
-
 /*    exec_command_utils.c    */
-
 //Function for exec_general
-void	parent_process(int *pipe_fd, pid_t pid, t_tree *node, t_shell_config *config);
-void	child_process(int *pipe_fd, t_tree *node, char **cmd_argv, t_shell_config *config);
+void	parent_process(int *pipe_fd, pid_t pid, t_tree *node, \
+			t_shell_config *config);
+void	child_process(int *pipe_fd, t_tree *node, char **cmd_argv, \
+			t_shell_config *config);
 void	exec_command(char **cmd_argv, t_shell_config *config);
 
 //others
